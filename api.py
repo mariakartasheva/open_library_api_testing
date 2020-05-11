391dfe86366b31e037d324e93e48c026e73be21d
mighty_maria <m.bezymyannaya.com>
2020-05-10 20:49:51 -0400

    Added base_url

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+base_url = "https://openlibrary.org/api/books"
+
 def get_parsed_body(url):
-    return json.loads(requests.get(url).text)
+    return requests.get(url).json()
 
 def get_text(url):
     return requests.get(url).text
--- test_books.py
+++ test_books.py
@@ -3,32 +3,34 @@ import json
 import pytest
 from .api import get_parsed_body
 from .api import get_text
+from .api import base_url
 
 
 class TestBibKeys():
+    @pytest.mark.dev
     @pytest.mark.parametrize('bib_key', ["ISBN:0451526538", "LCCN:96072233", "OCLC:36792831", "OLID:OL123M"])
     def test_get_the_book_by_bib_keys(self, bib_key):
         response = requests.get(
-            f"https://openlibrary.org/api/books?bibkeys={bib_key}")
+            f"{base_url}?bibkeys={bib_key}")
         assert response.status_code == 200, f"Status code for {bib_key} is wrong, expected 200, got {response.status_code}."
 
     def test_get_the_book_bib_keys_empty(self):
-        response = requests.get("https://openlibrary.org/api/books?bibkeys=")
+        response = requests.get(f"{base_url}?bibkeys=")
         assert response.status_code == 200, f"Status code is wrong. Empty bib_keys should return 200, got {response.status_code}."
 
     @pytest.mark.parametrize('bib_key1, bib_key2', [("ISBN:0451526538", "ISBN:0385472579"), ("LCCN:96072233", "LCCN:86072233"), ("OCLC:36792831", "OCLC:297222669"), ("OLID:OL123M", "OLID:OL124M"), ("ISBN:0451526538", "LCCN:86072233")])
     def test_get_different_books(self, bib_key1, bib_key2):
         first_book = get_parsed_body(
-            f"https://openlibrary.org/api/books?bibkeys={bib_key1}&format=json")
+            f"{base_url}?bibkeys={bib_key1}&format=json")
         second_book = get_parsed_body(
-            f"https://openlibrary.org/api/books?bibkeys={bib_key2}&format=json")
+            f"{base_url}?bibkeys={bib_key2}&format=json")
         assert first_book[bib_key1]["bib_key"] != second_book[bib_key2][
             "bib_key"], "Books in responses are the same, should be different."
 
     @pytest.mark.parametrize('bib_key', ["ISBN:0451526538", "LCCN:96072233", "OCLC:36792831", "OLID:OL123M"])
     def test_response_has_the_right_book(self, bib_key):
         book = get_parsed_body(
-            f"https://openlibrary.org/api/books?bibkeys={bib_key}&format=json")
+            f"{base_url}?bibkeys={bib_key}&format=json")
         response_bib_key = book[bib_key]["bib_key"]
         assert response_bib_key == bib_key, f'Got the wrong book: should be {bib_key}, got {response_bib_key}.'
 
@@ -37,17 +39,17 @@ class TestResponseFormat():
     @pytest.mark.parametrize('response_format', ['json', 'javascript'])
     def test_all_response_format_return_200(self, response_format):
         response = requests.get(
-            f"https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format={response_format}")
+            f"{base_url}?bibkeys=ISBN:0451526538&format={response_format}")
         assert response.status_code == 200, f"Status code for {response_format} is wrong, expected 200, got {response.status_code}."
 
     def test_wrong_response_format_returns_200(self):
         response = requests.get(
-            "https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&format=xml")
+            f"{base_url}?bibkeys=ISBN:0451526538&format=xml")
         assert response.status_code == 200, f"Expected status code for wrong response format is 200, got {response.status_code}."
 
     def test_no_format_returns_javascript_by_default(self):
         response = get_text(
-            "https://openlibrary.org/api/books?bibkeys=ISBN:0451526538")
+            f"{base_url}?bibkeys=ISBN:0451526538")
         assert response[0:3] == 'var', "Response format is wrong. Expected default response format is javascript"
 
 
@@ -55,12 +57,12 @@ class TestDataFormat():
     @pytest.mark.parametrize('data_format', ['viewapi', 'data', 'details'])
     def test_all_data_formats_return_200(self, data_format):
         response = requests.get(
-            f"https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&jscmd={data_format}")
+            f"{base_url}?bibkeys=ISBN:0451526538&jscmd={data_format}")
         assert response.status_code == 200, f"Status code for {data_format} is wrong, expected 200, got {response.status_code}."
 
     def test_wrong_data_format_returns_200(self):
         response = requests.get(
-            "https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&jscmd=test")
+            f"{base_url}?bibkeys=ISBN:0451526538&jscmd=test")
         assert response.status_code == 200, f"Expected status code for wrong data format is 200, got {response.status_code}."
 
 
@@ -71,6 +73,5 @@ def test_get_the_book_wrong_request():
 
 
 def test_get_the_book_with_no_parameters():
-    response = requests.get(
-        "https://openlibrary.org/api/books")
+    response = requests.get(base_url)
     assert response.status_code == 200, f"Status code for request with no parameters is wrong, expected 200, got {response.status_code}."
